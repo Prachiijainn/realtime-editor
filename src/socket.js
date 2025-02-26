ecea94d5751f54d0d88ed06c010b34adb76fcdff
prachiijainn <prachiijainn1@gmail.com>
2025-02-26 20:00:45 +0530

    made some changes in socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,11 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_SERVER_URL = 'http://localhost:3333'; // Change this if your backend runs on a different port
-
-export const initSocket = async () =>{
-    return io(SOCKET_SERVER_URL, {
-        transports: ['websocket'],  // Use WebSocket transport
-        reconnectionAttempts: 5,    // Retry 5 times
-        timeout: 10000,             // 10 seconds timeout
-    });
+export const initSocket = async () => {
+    const options = {
+        'force new connection': true,
+        reconnectionAttempt: 'Infinity',
+        timeout: 10000,
+        transports: ['websocket'],
+    };
+    return io(process.env.REACT_APP_BACKEND_URL, options);
 };
